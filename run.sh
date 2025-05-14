8a19ddd83c05840f644f815c3d64df9749bd737f
snimu <sebastian.nicolas.mueller@gmail.com>
2025-05-14 15:38:43 +0200

    install dependencies manually

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-pip install -e .
 export TASKS="truthfulqa,piqa,hellaswag,glue,lambada,arithmetic,mmlu"
 
 lm-eval --model-name MoT --model_args "name=snimu/MoT_pad-lr_pull-nn_bpt-16_how-noop-noop_BTMdim-1024-1024-1024_niter-100000_773322" --num-fewshot 3 --batch_size="auto" --max_batch_size=512 --output_path="results/noop-noop-1024-1024-1024"
