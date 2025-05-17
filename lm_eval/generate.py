1f918f9f72a7c39fdadcdd5ac3036af06c54e8b5
snimu <sebastian.nicolas.mueller@gmail.com>
2025-05-17 14:29:09 +0200

    warm up & save time per generation

--- lm_eval/generate.py
+++ lm_eval/generate.py
@@ -18,34 +18,42 @@ def get_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
+def make_instance(query: str, max_gen_toks: int) -> Instance:
+    return Instance(
+        "generate_until",
+        doc=dict(),
+        arguments=(query, dict(max_gen_toks=max_gen_toks)),
+        idx=0,
+        metadata=(None, None, None),
+        resps=[],
+        filtered_resps={},
+    )
+
+
 def main():
     args = get_args()
     model = MoTModel(args.name, temperature=args.temperature)
+    print("Warming up...")
+    model.generate_until([make_instance("Hello, my name is", 10)])
     while True:
         query = input("\n\n New, independent Query: ")
         if not query:
             break
         print("\n\n")
-        instance = Instance(
-            "generate_until",
-            doc=dict(),
-            arguments=(query, dict(max_gen_toks=args.max_gen_toks)),
-            idx=0,
-            metadata=(None, None, None),
-            resps=[],
-            filtered_resps={},
-        )
-        t0 = perf_counter()
-        responses = model.generate_until([instance] * args.num_samples)
-        t1 = perf_counter()
-        results = [{"query": query, "responses": responses, "time": t1-t0}]
+        results = [{"query": query, "responses": [], "times": []}]
+        for _ in range(args.num_samples):
+            t0 = perf_counter()
+            response = model.generate_until([make_instance(query, args.max_gen_toks)]).pop()
+            t1 = perf_counter()
+            results[-1]["responses"].append(response)
+            results[-1]["times"].append(t1-t0)
         if args.to_file:
             if os.path.exists(args.to_file):
                 with open(args.to_file, "r") as f:
                     results = json.loads(f.read()) + results
             with open(args.to_file, "w") as f:
                 f.write(json.dumps(results, indent=2))
-        for response in responses:
+        for response in results[-1]["responses"]:
             print("\n\n" + response)
         print(f"\n\nIn {(t1-t0):.2f} seconds")
 
